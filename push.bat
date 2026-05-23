93cea0d0ff31f627611cbf53889b14bfc52cc172
Harshil Patel <harshilpatelpilot@gmail.com>
2026-05-23 12:08:29 +0530

    chore: remove unused push.bat setup script

--- push.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit: VisionID AI"
-git branch -M main
-git remote add origin https://github.com/harshilpatel350/VisionID-AI.git
-git push -u origin main
